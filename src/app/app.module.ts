e5e2343eb5ae579a87f081fba71c7823e8af5c8a
Lonnie Johnson <lonnie.e.johnson@gmail.com>
2023-07-08 04:08:07 -0500

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StylistsComponent } from './components/stylists/stylists.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StylistsComponent,
+    ServicesComponent,
+    AppointmentComponent,
+    SummaryComponent,
+    PaymentComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
